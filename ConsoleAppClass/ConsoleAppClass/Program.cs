d6955786ff6f3cc9a4d9f548fe7b6c31be96b91d
CristisGit <cristiduta88@gmail.com>
2021-12-09 18:53:32 +0200

    first class and push to git

---
+++ ConsoleAppClass/ConsoleAppClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! facem clase");
+        }
+    }
+}
