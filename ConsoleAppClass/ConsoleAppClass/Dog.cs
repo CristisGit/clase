ed0fc4adb57680aec752be69eeadbf32be47c9a8
CristisGit <cristiduta88@gmail.com>
2021-12-09 20:50:47 +0200

    course finish

---
+++ ConsoleAppClass/ConsoleAppClass/Dog.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppClass
+{
+    class Dog
+    {
+        // parametreless constructor
+        //public Dog()
+        //{ 
+
+        //}
+
+        // constructorul indica cat de sanatoasa e clasa
+
+        // constructorul un fel de metoda care nu returneaza o valoare
+
+        // fields
+        // is a way to maintain state at the calss level
+        // usualy it should be private to the calss
+        // a fild are accesibile from the calss no matter the access modifier.
+
+        //public string _name;
+        private string _name; // fildurile ar trebuii sa fie private
+        //public readonly DateTime _birthDate;
+        private readonly DateTime _birthDate; // fildurile ar trebuii sa fie private
+
+        // readonly se initializeaza in constructor
+        // readonly valoarea nu se mai poate schimba nici macar in Class)
+        string _owner;
+        //public const decimal PI = 3.141516M;
+        private const decimal PI = 3.141516M;// fildurile ar trebuii sa fie private
+
+        // constanta constanta trebuie definita cand o declari;
+        //    myDog._birthDate = DateTime.Now.AddDays(100);
+        // constanta  modificat la readonly drept urmare nu poate fi modificat din exterior
+        //myDog.PI  nu fuctioneaza ca readonly
+        // se acceseaza prin TYPE <dog.>
+
+        public static int numberOfInstances;
+        public Dog(string name, string owner)
+        {
+          
+            _name = name;
+            _owner = owner;
+            _birthDate = DateTime.Now;
+            numberOfInstances++;
+        }
+
+        public Dog(string name)
+        {
+
+        }
+
+        public void Bark()
+        {
+
+            string name = "local name";
+
+            Console.WriteLine("ham ham");
+         //   PI = 4.15;
+
+          }
+
+        public static int GetTotalInstancessCreated()
+        {
+
+            return numberOfInstances;
+
+        }
+    }
+}
--- ConsoleAppClass/ConsoleAppClass/Program.cs
+++ ConsoleAppClass/ConsoleAppClass/Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+// clasele au proprietati <universal valabile>
+// clasele au ?comportamente? care <universal valabile>
+//a constructor is a "special" method hat helps with the obj ceation(instantiation),
+//public class Animal
+//////{
+//////  public animal()
+//////////{
+//////////
+//////////}
+//////}
+// if we don't specify a parameterless constructor the compiler generates one for us
+//
 
+// ab object is a realization of a class. eg stamo is the template
+// and the mark of the stamp is the 'object' 
+// eg. Book vs shelf
+// eg new Dog();
 namespace ConsoleAppClass
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! facem clase");
+            //Dog dog = new Dog();
+
+            Dog myDog = new Dog("Grivei", "Maricica");
+            // myDog._name = "Azorel"; //'Dog._name' is inaccessible due to its protection level 
+
+
+            Console.WriteLine(Dog.GetTotalInstancessCreated());
+            //    myDog._birthDate = DateTime.Now.AddDays(100);
+            // readonly se initializeaza in constructor
+            // readonly valoarea nu se mai poate schimba nici macar in Class)
+
+            // - modificat la readonly drept urmare nu poate fi modificat din exterior
+            //myDog.PI  nu fuctioneaza ca readonly
+            // se acceseaza prin TYPE <dog.>
+
+            Dog YourDog = new Dog("Puffy", "Cristian");
+            //Console.WriteLine(Dog.numberOfInstances);
+            Console.WriteLine(Dog.GetTotalInstancessCreated());
+
+
+           // decimal pidog = Dog.PI;//'Dog._name' is inaccessible due to its protection level 
+            //decimal yourDog = pidog * 2; 
+          
+
+            // access modifier
+
+            // public
+
+            // private
+
+            //protected
+
+            //internal - limits the acces to the current assembly (la namespace)(eg same project)
         }
     }
 }
